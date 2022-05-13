f8b9401b07a1d4e48e8ec9c0243f5f1f550d32e1
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2022-05-13 17:33:44 -0300

    Create stack.h

---
+++ src/stack.h
@@ -0,0 +1,13 @@
+/*
+ * File : stack.h
+ * Author: Guilherme Monteiro de Oliveira
+ * Data: 02/05/22
+ * Description: Implementation of the stack data structure.
+ */
+
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+
+
+#endif //STACK_H_INCLUDED
