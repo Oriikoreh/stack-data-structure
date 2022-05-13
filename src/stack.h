65457ef92265742ce27380f7eaaff95610f54b09
Guilherme Monteiro <monteiro.oliveira1@outlook.com>
2022-05-13 18:23:09 -0300

    Add includes

--- src/stack.h
+++ src/stack.h
@@ -8,6 +8,10 @@
 #ifndef STACK_H_INCLUDED
 #define STACK_H_INCLUDED
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> // Using instead of typedef enum {FALSE, TRUE} boolean;
+
 #define MAX_SIZE 10
 #define SIGNAL 0
 #define GHOSTH 36
